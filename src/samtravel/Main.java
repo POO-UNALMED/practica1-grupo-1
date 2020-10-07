5934ff7f1a734037ae6cb9b02e9500a752f60373
CarlosMarioCA <Camchavezag@unal.edu.co>
2020-10-07 16:19:20 -0500

    Prueba 1

--- src/samtravel/Main.java
+++ src/samtravel/Main.java
@@ -4,8 +4,14 @@ import java.util.*;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("imprimir");
-
+		System.out.println(" ");
+		System.out.println(" ");
+		System.out.println("----- BIENVENIDOS A SAM-TRAVEL -----");
+		System.out.println(" ");
+		System.out.println(" ");
+		System.out.println("Estamos aquí para ofrecerles la mejor experiencia de viaje posible");
+		System.out.println("¿Es usted un usuari@ registrad@ en nuestro sistema?");
+		System.out.println(" ");
 	}
 
 }
